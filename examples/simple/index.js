ca0a83ba0120837cbcd7701e14a0e1de757acb99
fragment0 <hi@fragment0.com>
2018-04-10 17:35:35 +0800

    update styles

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -22,7 +22,7 @@ class App extends React.Component {
             <img alt="" src="https://images.unsplash.com/photo-1522198734915-76c764a8454d?auto=format&fit=crop&w=500&q=60" />
             <YouTubeNextVideo
               author="CaseyNeistat"
-              title="SHE'S MUCH BETTER'"
+              title="SHE'S MUCH BETTER yo yo yo yo yo yo yo yo yo yo yo yo"
               onNext={this.handleNext}
               onCancel={this.handleCancel}
               locale={locale}
